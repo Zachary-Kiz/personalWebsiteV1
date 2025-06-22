ca608b7eaac64280852e9d01a0a29b469f743eec
Zachary-Kiz <zachary.kizell@gmail.com>
2025-06-22 14:50:21 -0400

    heroku

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root : '.',
   plugins: [react()],
+  build: {
+    outDir: '../dist',  // Where to output the build (important for Heroku)
+    emptyOutDir: true
+  }
 })
